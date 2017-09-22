46287d14f665baf964f5358f2ab23a79de9c2ed6
unknown <maciekslotwinski@gmail.com>
2017-09-22 08:42:01 +0200

    poprawiony div, wyswietlanie w html zamiast czystego jsona

--- custom.js
+++ custom.js
@@ -1,14 +1,26 @@
-$(function(){
-	
-	$("#downloadData").click(function(){
-		console.log('click');
-    $.ajax({url: "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", success: function(result){
-        $("#devData").html(result);
-		console.log(result);
-    }});
-	
-	});
-	
-	
-	
+$(function () {
+    'use strict';
+
+    $("#downloadData").click(function () {
+        $.ajax({
+            url: "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
+            success: function (result) {
+
+                var developerData = "imie: " + result.imie + "<br>";
+                developerData += "nazwisko: " + result.nazwisko + "<br>";
+                developerData += "zawod: " + result.zawod + "<br>";
+                developerData += "firma: " + result.firma + "<br>";
+
+                $("#dane-programisty").html(developerData);
+
+            },
+            dataType: "json"
+
+
+        });
+
+    });
+
+
+
 });
